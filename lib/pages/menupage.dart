f8788d11ee5d6c712daec0f645904deb822618bc
Siwar Soula <siwarsoula@gmail.com>
2024-02-27 17:33:08 +0100

    displaying data from the json

--- lib/pages/menupage.dart
+++ lib/pages/menupage.dart
@@ -8,13 +8,50 @@ class MenuPage extends StatelessWidget {
   const MenuPage({super.key, required this.dataManager});
   @override
   Widget build(BuildContext context) {
-    var p = Product(id: 1, name: "Dummy product", price: 1.25, image: "");
-    return ListView(
-      children: [
-        MenuItem(product: p, onAdd: (){},),
-        MenuItem(product: p, onAdd: (){},),
-        MenuItem(product: p, onAdd: (){},),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: FutureBuilder<List<Category>>(
+        future: dataManager.getMenu(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  // EACH CATEGORY STARTS HERE
+                  var category = snapshot.data![index];
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 32.0, bottom: 8.0, left: 8.0),
+                        child: Text(
+                          category.name,
+                          style: TextStyle(color: Colors.brown.shade400),
+                        ),
+                      ),
+                        ListView.builder(
+                          shrinkWrap: true,
+                          physics: const ClampingScrollPhysics(),
+                          itemCount: category.products.length,
+                          itemBuilder: (context, index) {
+                            return MenuItem(
+                              product: category.products[index],
+                              onAdd: (p) => dataManager.cartAdd(p),
+                            );
+                          },
+                        )
+                      
+                    ],
+                  );
+                });
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+          // By default, show a loading spinner.
+          return const CircularProgressIndicator();
+        },
+      ),
     );
   }
 }
