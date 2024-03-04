484afeac45b407421300dad16135a2b03b00d529
Siwar Soula <siwarsoula@gmail.com>
2024-03-04 12:24:40 +0100

    displaying Obs variables within Obx

--- lib/pages/testgestx.dart
+++ lib/pages/testgestx.dart
@@ -20,68 +20,137 @@ class TestGetX extends StatelessWidget {
       body: SizedBox(
         width: double.maxFinite,
         height: double.maxFinite,
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              GetBuilder<TapController>(builder: (_){
-                return Container(
-                  margin: const EdgeInsets.all(10),
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
+        child: Column(
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GetBuilder<TapController>(builder: (_){
+                    return Container(
+                      margin: const EdgeInsets.all(10),
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Center(
+                        child: Text(controller.x.toString(), 
+                        style:const TextStyle(
+                          fontSize: 20,
+                          color: Colors.brown,
+                        ),),
+                      ),
+                    );
+                    },
                   ),
-                  child: Center(
-                    child: Text(controller.x.toString(), 
-                    style:const TextStyle(
-                      fontSize: 20,
-                      color: Colors.brown,
-                    ),),
+                  GestureDetector(
+                    onTap: (){
+                      controller.increaseX();
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.all(20),
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.black12,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Center(
+                        child: Text("x ++", style: TextStyle(
+                          fontSize: 20,
+                        ),),
+                      ),
+                    )
                   ),
-                );
-                },
-              ),
-              GestureDetector(
-                onTap: (){
-                  controller.increaseX();
-                },
-                child: Container(
-                  margin: const EdgeInsets.all(10),
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: Colors.black12,
-                    borderRadius: BorderRadius.circular(20),
+                  GestureDetector(
+                    onTap: (){
+                      controller.decreaseX();
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.all(20),
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.black12,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Center(
+                        child: Text("x --", style: TextStyle(
+                          fontSize: 20,
+                        ),),
+                      ),
+                    )
                   ),
-                  child: const Center(
-                    child: Text("x ++", style: TextStyle(
-                      fontSize: 20,
-                    ),),
+                ]),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Obx(() =>
+                    Container(
+                      margin: const EdgeInsets.all(10),
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Center(
+                        child: Text(controller.y.value.toString(), 
+                        style:const TextStyle(
+                          fontSize: 20,
+                          color: Colors.brown,
+                        ),),
+                      ),
+                    ),
                   ),
-                )
-              ),
-              GestureDetector(
-                onTap: (){
-                  controller.decreaseX();
-                },
-                child: Container(
-                  margin: const EdgeInsets.all(10),
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: Colors.black12,
-                    borderRadius: BorderRadius.circular(20),
+                  
+                  GestureDetector(
+                    onTap: (){
+                      controller.increaseY();
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.all(20),
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.brown,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Center(
+                        child: Text("y ++", style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),),
+                      ),
+                    )
                   ),
-                  child: const Center(
-                    child: Text("x --", style: TextStyle(
-                      fontSize: 20,
-                    ),),
+                  GestureDetector(
+                    onTap: (){
+                      controller.decreaseY();
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.all(20),
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.brown,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Center(
+                        child: Text("y --", style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),),
+                      ),
+                    )
                   ),
-                )
-              ),
-            ]),
+                ]),
+            ),
+          ],
         ),
       ),
     );
